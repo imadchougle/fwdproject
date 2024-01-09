58a4618689e9309e916b2d22f16327a08312166a
imadchougle <imadchougle2@gmail.com>
2024-01-09 13:16:55 +0530

    New script for LTP in range

--- Ltp_Fib_Range.py
+++ Ltp_Fib_Range.py
@@ -1,13 +1,36 @@
-stock_price = 629.3
+def find_fibonacci_range(stock_name, stock_price, fib_levels):
+    fibonacci_ranges = {
+        '0 - 0.236': (fib_levels[0], fib_levels[1]),
+        '0.236 - 0.382': (fib_levels[1], fib_levels[2]),
+        '0.382 - 0.5': (fib_levels[2], fib_levels[3]),
+        '0.5 - 0.618': (fib_levels[3], fib_levels[4]),
+        '0.618 - 0.786': (fib_levels[4], fib_levels[5]),
+        '0.786 - 1': (fib_levels[5], fib_levels[6]),
+        '1 - 1.236': (fib_levels[6], fib_levels[7]),
+        '1.236 - 1.618': (fib_levels[7], fib_levels[8]),
+        '1.618 + ': (fib_levels[8], float('inf'))
+    }
 
-fib_0 = 515.55
-fib_0_236 = 549.99
-fib_0_382 = 571.3
-fib_0_5 = 588.52
-fib_0_618 = 605.75
-fib_0_786 = 630.27
-fib_1 = 661.5
-fib_1_236 = 695.94
-fib_1_618 = 751.7
+    for range_name, bounds in fibonacci_ranges.items():
+        lower, upper = bounds
+        if lower <= stock_price <= upper:
+            return f"For {stock_name}, the stock price {stock_price} is in {range_name}"
 
-fib_price_list = [fib_0, ]
+    return f"For {stock_name}, the stock price {stock_price} is not in any Fibonacci range"
+
+
+stocks = ["XYZ", "ABC", "123", "PNB"]
+stock_prices = [620, 910, 450, 97.5]
+
+fib_levels_stocks = [
+    (500, 550, 580, 600, 620, 650, 680, 700, 750),
+    (700, 750, 780, 800, 820, 850, 880, 900, 950),
+    (400, 450, 480, 500, 520, 550, 580, 600, 650),
+    (60, 90, 100, 120, 100, 90, 50, 90, 50)
+]
+
+for i in range(len(stocks)):
+    result = find_fibonacci_range(stocks[i],
+                                  stock_prices[i],
+                                  fib_levels_stocks[i])
+    print(result)
